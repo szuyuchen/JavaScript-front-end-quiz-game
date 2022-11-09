4b5b5bf0dee60b86c1e8118445ae8fc90be8430d
Cathy <szuyuchenv@gmail.com>
2022-11-09 22:59:00 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const question = 'HTML stand for ?';
+const answers = [
+  'Hypermark Language',
+  'Hypermix language', 
+  'Hypertext Markup Language',
+  'Hypertension Language'
+];
+const correct = 'Hypertext Markup Language';
